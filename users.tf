42a0f94f1673f197b2a21660e48ca42bfdf8c69d
Marcelo Ochoa <mochoa.ext@andromedalatam.com>
2022-06-16 10:19:56 -0300

    added required variable user_email

--- users.tf
+++ users.tf
@@ -7,6 +7,7 @@ resource "oci_identity_user" "oci_user" {
     compartment_id = var.tenancy_ocid
     description = "Swarm User to access Cloud Storage"
     name = "swarm-${random_string.deploy_id.result}"
+    email = var.user_email
 }
 
 resource "oci_identity_user_capabilities_management" "oci_user" {
